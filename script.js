de863c97aa89d85a063b4bad647bed111dbe7bb7
Onesimus <ocpradhan@gmail.com>
2022-11-04 23:27:52 +0530

    Created a function called playRound for comparing player and computer selection

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-"use: strict";
-
+// For Randomnly returning Rock, Paper and Scissor.
 const getComputerChoice = function () {
   const choices = ["Rock", "Paper", "Scissor"];
 
-  return choices[Math.floor(Math.random() * choices.length)];
+  return choices[Math.floor(Math.random() * choices.length)].toLowerCase();
 };
+
+// For comparing the The selection of the player vs. computer Selection
+function playRound(playerSelection, computerSelection) {
+  const lowerPlayerSelection = playerSelection.toLowerCase();
+
+  if (lowerPlayerSelection === computerSelection) {
+    return `It's a Tie!`;
+  }
+
+  if (lowerPlayerSelection !== computerSelection) {
+    if (computerSelection === "paper") {
+      return `You Lose! Paper beats Rock`;
+    } else if (computerSelection === "scissor") {
+      return `You Win! Rock beats scissor`;
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
